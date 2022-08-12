620fcd38da486da8dfe16f848af1617cdae731a2
Angelo <angelofag3095@gmail.com>
2022-08-12 09:39:12 +0200

    -Correzione del file application.yml e del NameController

--- src/main/java/it/develhope/deploy01/controller/NameController.java
+++ src/main/java/it/develhope/deploy01/controller/NameController.java
@@ -1,12 +1,29 @@
 package it.develhope.deploy01.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class NameController {
-    @GetMapping
-    public String getName(){
-        return "Angelo Fagiani";
+
+    @Autowired
+    Environment environment;
+
+    @Value("${myCustomVarTree.devName}")
+    String myCustomVar;
+
+
+
+    @GetMapping("/")
+    public String getDevName(){
+        String var = environment.getProperty("myCustomVarTree.devName");
+        return var;
     }
+
+
 }
